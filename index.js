e36f04357c1f8e06026ef77fd0955e823b856b2d
Stephen O'Kennedy <stephenoken@gmail.com>
2015-07-06 20:45:17 +0100

    Refactored the regular expression.

--- index.js
+++ index.js
@@ -5,25 +5,31 @@ const PLUGIN_NAME = 'gulp-remove-html';
 
 function gulpRemoveHtml(){
     var stream = through2.obj(function(chunk, enc, cb){
+        //Does not support Streams
        if(chunk.isStream()){
            this.emit('error', new PluginError(PLUGIN_NAME,'Streams not supported'));
            return cb();
        }
-
         if(chunk.isBuffer()){
             var fileContents = chunk.contents.toString();
-            //TODO refactor the patterns
-            var dejectStartPattern =/(<!--\s*<([Deject]+)>\s*-->)/ig;
-            var dejectEndPattern =/(<!--\s*<(\/[Deject]+)>\s*-->)/ig;
+            /*
+            The regular expression searches for a pattern that matches the open closing tags of deject.
+            NOTE That these tags must be commented out for the plugin to reconise them.
+            */
+            var dejectPatternRegex =/<!--\s*<(?:\/)?[Deject]+>\s*-->/ig;
 
-            var dejectStartIndex = dejectStartPattern.exec(fileContents);
-            if(dejectStartIndex == null){
+            var dejectResult = dejectPatternRegex.exec(fileContents);
+            if(dejectResult == null){
                 this.emit('error', new PluginError(PLUGIN_NAME, 'File does not contain the right tag'));
                 return cb();
             }
-            dejectEndPattern.exec(fileContents);
-            var processedFile = fileContents.substring(0,dejectStartIndex.index);
-            processedFile = processedFile.concat(fileContents.substring(dejectEndPattern.lastIndex,fileContents.length));
+            /*
+            The exec() method is called a second time to find the closing tag.
+            */
+            dejectPatternRegex.exec(fileContents);
+            
+            var processedFile = fileContents.substring(0,dejectResult.index);
+            processedFile = processedFile.concat(fileContents.substring(dejectPatternRegex.lastIndex,fileContents.length));
             chunk.contents = new Buffer(processedFile);
         }
         this.push(chunk);
